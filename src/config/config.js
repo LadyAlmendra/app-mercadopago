68258e6f78654a504d9d314f47d0f72a438517df
Almendra Romina R <almendraromina1@gmail.com>
2023-09-22 02:09:46 -0300

    HOTS Render

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,8 @@ export const PORT = process.env.PORT || 8000;
 // URL del front
 export const HOTS = process.env.HOTS;
 
+export const HOTS2 = process.env.HOTS2
+
 // Datos de deconexión con de mercadopago
 export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 export const INTEGRATOR_ID = process.env.INTEGRATOR_ID;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import payments from "./payment.routes.js";
+import { HOTS2 } from "../config/config.js";
 
 const router = Router();
 router.get('/', (req, res) => {
+    console.log(HOTS2)
     res.send(`
     <div style="text-align: center; padding: 1rem;">
         <h1>Pagar con mercadopago</h1> 
-        <a href="http://localhost:3000/payment-mercadopago/create-order">Crear orden</a>    
+        <a href="${HOTS2}/payment-mercadopago/create-order">Crear orden</a>    
     </div>`);
 });
 
